6fbb79b026e5ea6bd2605767d32fea0d1d85a9a3
your name here <samuelchavez@students.berkeley.net>
2019-03-18 13:51:52 -0700

    initial commit1

--- src/QuestionGroup.java
+++ src/QuestionGroup.java
@@ -1,2 +1,14 @@
 public class QuestionGroup {
+    Question[] easy;
+    Question[] medium;
+    Question[] hard;
+    int gameLength = 10;
+
+    public void createEasy() {
+        easy[0] = new Question("what is ", 7, 2);
+    }
+
+    public Question() {
+            createEasy();
+        }
 }
